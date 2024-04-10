fff0b69e93e2e9d6c139284f3c5eb2f23ba2f15d
Portablejim <portablejim@jamesmckay.id.au>
2024-04-11 09:00:17 +1000

    nvim: enable lsp code action keymap

--- nvim/nvim/lua/plugins/lsp-config.lua
+++ nvim/nvim/lua/plugins/lsp-config.lua
@@ -52,7 +52,7 @@ return {
 
 			--vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 			--vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-			--vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+			vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
 			-- Global mappings.
 			-- See `:help vim.diagnostic.*` for documentation on any of the below functions
 			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
